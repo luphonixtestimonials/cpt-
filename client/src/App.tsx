b222c2d079720a1313dbb521260b4af4b690e273
Dhyey Khanpara <106962485+dhyeykhanpara21@users.noreply.github.com>
2025-10-31 14:51:47 +0530

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 // Reference: Replit Auth integration blueprint
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
@@ -256,12 +254,10 @@ function AppContent() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <AppContent />
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <AppContent />
+      <Toaster />
+    </TooltipProvider>
   );
 }
 
